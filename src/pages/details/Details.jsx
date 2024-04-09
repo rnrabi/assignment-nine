26d88d6963b0d6ec762d0270f10eb2983b97b18e
rnrabi <rnrabi913@gmail.com>
2024-04-09 02:21:57 -0700

    details page is ok

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { FaCheck } from "react-icons/fa6";
 
 const Details = () => {
     const allData = useLoaderData()
@@ -19,8 +20,30 @@ const Details = () => {
                     <div className="col-span-2">
                         <h1 className="text-3xl">{title}</h1>
                         <p className="py-6">{description}</p>
-                        {/* কিছু কাজ বাকি আছে , উলিপুর থেকে এসে করবো। */}
-                        <button className="btn btn-primary">Get Started</button>
+                        <div className="flex justify-between">
+                            <div className="w-1/2">
+                                <h2 className="text-xl text-gray-400 font-semibold flex items-center"> <FaCheck className="text-green-400"></FaCheck> {segment_name}</h2>
+
+                                <h2 className="mt-4 flex items-baseline">  <span className="text-xl flex"><FaCheck className="text-green-400"></FaCheck>Location:</span> <span className="text-xl text-gray-400 font-semibold ">{location}</span></h2>
+
+                                <h2 className="mt-4 flex items-center"><FaCheck className="text-green-400"></FaCheck><span className="text-xl">Area:</span> <span className="text-xl text-gray-400 font-semibold ">{area}sq ft</span></h2>
+                             
+                                <h2 className="mt-4 flex items-center"><FaCheck className="text-green-400"></FaCheck><span className="text-xl">Price:</span> <span className="text-xl text-gray-400 font-semibold ">{price}tk</span></h2>
+
+                                <h2 className="mt-4 flex items-center"><FaCheck className="text-green-400"></FaCheck><span className="text-xl">Status: </span> <span className="text-xl text-gray-400 font-semibold ">{status}</span></h2>
+
+                            </div>
+                            <div>
+                                <h2 className="text-2xl text-gray-400 font-semibold underline mb-4 w-full">Our facilities</h2>
+                                {
+                                    facilities.map((facility , index)=><h2
+                                    className="text-xl"
+                                    key={index}
+                                    ><span>{index+1}.</span> {facility}</h2>)
+                                }
+                            </div>
+                        </div>
+                        {/* <button className="btn btn-primary">Get Started</button> */}
                     </div>
                 </div>
             </div>
