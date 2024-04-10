8af89195030123c17ace4196f28b6d40f987eaa0
rnrabi <rnrabi913@gmail.com>
2024-04-10 02:52:45 -0700

    all is done

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,4 @@
-// import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -11,10 +8,6 @@ import slide1 from '../../assets/slider1.jpg';
 import slide2 from '../../assets/slider2.jpg';
 import slide3 from '../../assets/slider3.jpg';
 import slide4 from '../../assets/slider4.jpg';
-
-
-
-// import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
 const Slider = () => {
@@ -29,8 +22,11 @@ const Slider = () => {
         className="mySwiper"
       >
         <SwiperSlide><img className='w-full md:h-[500px]' src={slide1} alt="" /></SwiperSlide>
+
         <SwiperSlide><img className='w-full md:h-[500px]' src={slide2} alt="" /></SwiperSlide>
+
         <SwiperSlide><img className='w-full md:h-[500px]' src={slide3} alt="" /></SwiperSlide>
+
         <SwiperSlide><img className='w-full md:h-[500px]' src={slide4} alt="" /></SwiperSlide>
       </Swiper>
     </div>
